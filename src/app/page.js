aaf7a495ff9c5ab7b91bf35aa312b09ba3ff6950
chakorn.san <chakorn.san@cpf.co.th>
2024-04-18 14:30:27 +0700

    feat : add middleware

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-row gap-10">
       <div>Home</div>
-      <Link href="/blog/1/2">Blog 1</Link>
+      <Link href="/blog/1">Blog 1</Link>
       <Link href="/blog/1/2">Blog 1 id 2</Link>
       <Link href="/dashboard">Dashboard</Link>
     </div>
